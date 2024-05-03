5928411e7fbb620f1ac9260ec90163d2d5c63d49
Murilo20H <murilohenrique123correa@gmail.com>
2024-05-03 16:19:39 -0300

    Alterando página de evolução e criando página de objetivos

---
+++ SIte/Tela Principal - Deslogado/Objetivos/objetivos.js
@@ -0,0 +1,24 @@
+var pagina = 0;
+function alternar() {
+    if (pagina == 1) {
+        div_usos_tecnologia.style = "transition: 0.5s; left: 100vw"
+        botao_seta.style = "display: none";
+        pagina = 0;
+    } else if (pagina == 2) {
+        div_usos_tecnologia.style = "transition: 0.5s; left: 0vw"
+        botao_seta2.style = "display: flex";
+        pagina = 1;
+    }
+}
+
+function alternar2() {
+    if (pagina == 0) {
+        div_usos_tecnologia.style = "transition: 0.5s; left: 0vw"
+        botao_seta.style = "display: flex";
+        pagina = 1;
+    } else if (pagina == 1) {
+        div_usos_tecnologia.style = "transition: 0.5s; left: -100vw"
+        botao_seta2.style = "display: none";
+        pagina = 2
+    }
+}
