079f6a38198b13f284bafb46994a08cc02cefe80
Murilo20H <murilohenrique123correa@gmail.com>
2024-05-12 17:20:42 -0300

    Alterando botão das páginas e Criando página de linguagens

--- SIte/public/InicioLogado/inicio.js
+++ SIte/public/InicioLogado/inicio.js
@@ -1,17 +1,40 @@
-// HOME ANTES DE LOGAR
-function login() {
-    window.location.href = "../login.html";
+function sair() {
+    window.location.href = "../index.html";
+}
+
+function csharp() {
+    window.location.href = "../CSharp/csharp.html";
+}
+
+function java() {
+    window.location.href = "../Java/java.html";
+}
+
+function javascript() {
+    window.location.href = "../Javascript/javascript.html";
+}
+
+function html() {
+    window.location.href = "../LinguagemHTML/html.html";
+}
+
+function css() {
+    window.location.href = "../LinguagemCSS/css.html";
+}
+
+function sql() {
+    window.location.href = "../SQL/sql.html";
 }
 
 function sobre_mim() {
-    window.location.href = "../../Sobre Mim/sobre_mim.html";
+    window.location.href = "../Sobre Mim/sobre_mim.html";
 }
 
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var nome_usuario = document.getElementnomeById("nome_usuario");
+    var nome_usuario = document.getElementById("nome_usuario");
 
     if (email != null && nome != null) {
         nome_usuario.innerHTML = nome;
